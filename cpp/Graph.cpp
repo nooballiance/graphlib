0fe83716531e6f8e1296a517e4a2957efe9613d6
Akhilesh Dhaundiyal <akhilesh.dhaundiyal@gmail.com>
2017-10-26 22:14:44 +0530

    Add files via upload

--- cpp/Graph.cpp
+++ cpp/Graph.cpp
@@ -117,52 +117,231 @@ void Graph::SCC()
     }
 }
 
-//Finding path using BFS
+//Euler
 
-void Graph::find_path(int start, int end, int par[])
+void Graph :: Euler(int n, int e)
 {
-    if(start==end || end==-1)
-    {
-        cout<<start;
-    }
-    else    
-    {
-        find_path(start, par[end],par);
-        cout<<"->"<<end;
-    }
+	#define NMAX 100005
+	#define MMAX 700005
+
+	vector<int> G[ NMAX ];
+	stack<int> STACK;
+
+	int degree[ NMAX ],
+	sol[ MMAX ],
+	num_nodes = n, 
+	num_edges = e,
+	i,
+	x,
+	y,
+	a,
+	b;
+
+	for (i = 0; i < V; ++i)
+	{
+		x = i+1;
+
+		list<int>::iterator it;
+		for (it = adj[i].begin(); it != adj[i].end(); ++it)
+		{
+			y = *it;
+			y = y+1;
+
+			G[ x ].push_back( y );  
+			G[ y ].push_back( x );
+
+			degree[ x ]++;
+			degree[ y ]++;			
+		}
+	}
+
+	for(i = 1; i <= num_nodes; i++) 
+	{
+		if(degree[ i ] % 2) 
+		{
+		   cout<<"euler path not found \n";
+		   return;
+		}
+	}
+
+	STACK.push( 1 );
+
+	while(!STACK.empty()) 
+	{
+		a = STACK.top();
+
+	   if(!degree[ a ]) 
+	   {
+	       sol[++sol[0]] = a;
+	       STACK.pop();
+	       continue;  
+	   }
+
+	   b = G[ a ].back();
+
+	   G[ a ].pop_back();           
+
+	   degree[ a ]--;
+	   degree[ b ]--;
+
+	   STACK.push( b ); 
+
+	   for(vector<int>::iterator it = G[ b ].begin(); it != G[ b ].end(); ++it) 
+	   {
+	       if(*it == a) 
+	       {
+	           G[ b ].erase( it );
+	           break;
+	       }   
+	   } 
+	}
+
+	for(i = 1; i <= num_edges; i++) 
+	cout<<sol[ i ]<<" ";
+	cout<<endl; 
 }
 
-void Graph::BFS(int n, int src)
+//Hamilton
+
+bool Graph :: check_graph(int v) 
 {
-    queue <int> Q;
-    Q.push(src);
-    int visited[100] = {0};
-	int parent[100];
-
-    visited[src] = 0;
-
-    while(!Q.empty())
+    color[v] = 1;
+    for (size_t i = 0; i < gr[v].size(); ++i) 
     {
-        int u = Q.front();
+        int to = gr[v][i];
+        if (color[to] == 0) 
+        {
+            if (check_graph(to))
+                return true;
+        }
+        else if (color[to] == 1) 
+        {
+            cycle_start = to;
+            return true;
+        }
+    }
+    color[v] = 2;
+    return false;
+}
+  
+void Graph :: dfs(int v) 
+{
+    used[v] = true;
+    for (size_t i = 0; i < gr[v].size(); ++i) 
+    {
+        int to = gr[v][i];
+        if (!used[to])
+            dfs(to);
+    }
+    beat.push_back(v);
+}
+  
+void Graph :: topsort() 
+{
+    for (int i = 0; i < nodeCount; ++i)
+        used[i] = false;
+    beat.clear();
+    for (int i = 0; i < nodeCount; ++i)
+        if (!used[i])
+            dfs(i);
+    reverse(beat.begin(), beat.end());
+}
+  
+void Graph :: Hamilton(int n, int e)
+{  
+    int edgeCount;  
+    int primaryNode;
+    int secondaryNode;
+  
+    nodeCount = n;
+    edgeCount = e;
+  
+    vector<int> tmp;
+    gr.assign((unsigned long) nodeCount, tmp);
+    color.assign((unsigned long) nodeCount, 0);
+    used.assign((unsigned long) nodeCount, false);
 
-		list<int>::iterator i;
+	for (int i = 0; i < V; ++i)
+	{
+		primaryNode = i;
 
-		for (i = adj[u].begin(); i != adj[u].end(); ++i)
+		list<int>::iterator it;
+		for (it = adj[i].begin(); it != adj[i].end(); ++it)
 		{
-			int v = *i-1;
-            if(!visited[v])
-            {
-                parent[v] = u+1;
-                visited[v] = 1;
-                Q.push(v);
-			}
+			secondaryNode = *it;
+
+	        gr[primaryNode].push_back(secondaryNode);			
 		}
-		
-		Q.pop();
-    }    
+	}
+  
+    for (int i = 0; i < nodeCount; ++i)
+        if (check_graph(i))
+            break;
+  
+    bool exist = false;
+    if (cycle_start == -1) 
+    {
+        topsort();
+        for (int i = 0; i < beat.size() - 1; i++) 
+        {
+            for (int j = 0; j < gr[beat[i]].size(); j++) 
+            {
+                if (gr[beat[i]][j] == beat[i+1]) 
+                {
+                    exist = true;
+                    break;
+                }
+            };
+            if (!exist) 
+            {
+                cout << "not hamiltonian" << endl;
+                return;
+            };
+            exist = false;
+        };
+  
+        cout << "yes, hamiltonian" << endl;
+        return;
+    } 
+    else 
+    {
+        cout << "not hamiltonian" << endl;
+        return;
+    }
+  
+}
+
+//Topological Sort
+
+void Graph::topologicalSortUtil(int v, bool visited[], 
+                                stack<int> &Stack)
+{
+    visited[v] = true;
  
- 	int x,y;
-	cout<<"Enter Starting vertex and Ending vertex"<<endl;
-    cin>>x>>y;
-    find_path(x,y,parent);    
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            topologicalSortUtil(*i, visited, Stack);
+ 
+    Stack.push(v);
+}
+ 
+void Graph::topologicalSort()
+{
+    stack<int> Stack;
+ 
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    for (int i = 0; i < V; i++)
+      if (visited[i] == false)
+        topologicalSortUtil(i, visited, Stack);
+ 
+    while (Stack.empty() == false)
+    {
+        cout << Stack.top()+1 << " ";
+        Stack.pop();
+    }
+    cout << endl;
 }
--- cpp/WGraph.cpp
+++ cpp/WGraph.cpp
@@ -47,3 +47,47 @@ void WGraph :: printGraph()
 		cout<<endl;
 	}    
 }
+
+void WGraph :: Dijkstra(int s)
+{
+const int N = 1e5 + 5;
+const long long inf = 1e16 + 16;
+int n , k , x , m;
+long long dist[N];
+
+	for(int i = 0 ; i <= n ; ++i)
+		dist[i] = inf;
+	
+	dist[s] = 0;
+	priority_queue < pair < long long , int > > pq;
+	
+	while(!pq.empty())
+		pq.pop();
+	
+	pq.push({0 , s});
+	while(!pq.empty())
+	{
+		int node = pq.top().second;
+		long long cost = -pq.top().first;
+		pq.pop();
+		
+		if(dist[node] < cost)
+			continue;
+			
+		for(auto it : adj[node])
+		{
+			int next = it.first;
+			long long nd = it.second + cost;
+			
+			if(dist[next] > nd)
+			{
+				dist[next] = nd;
+				pq.push({-nd , next});
+			}
+		}
+	}
+	
+	for(int i = 0 ; i < V ; ++i)
+		cout<<s+1<<" -> "<<i+1<<" : "<<dist[i]<<endl;
+		
+}
